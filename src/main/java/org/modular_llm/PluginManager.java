26147fa910cca30342b20a249d33e302287a6469
Your Name <you@example.com>
2025-03-22 15:31:55 -0600

    Model-Plugin-Plugin Manager interface changes

--- src/main/java/org/modular_llm/PluginManager.java
+++ src/main/java/org/modular_llm/PluginManager.java
@@ -7,24 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PluginManager {
-    // Map to store plugin names and their corresponding Plugin objects
     private Map<String, Plugin> plugins = new HashMap<>();
 
-    // Loads a plugin from the src/plugins directory by its name
     public void loadPlugin(String pluginName) {
         try {
-            // Define the folder where plugins are located
             File pluginDir = new File("src/plugins");
             URL[] urls = { pluginDir.toURI().toURL() };
-
-            // Create a class loader for the plugin directory
             URLClassLoader loader = new URLClassLoader(urls);
 
-            // Load the class for the plugin. It is assumed that the class is in the "plugins" package.
             Class<?> clazz = loader.loadClass("plugins." + pluginName);
-            // Create a new instance of the plugin class
+
             Plugin plugin = (Plugin) clazz.getDeclaredConstructor().newInstance();
-            // Save the plugin in the map
             plugins.put(pluginName, plugin);
             System.out.println("Loaded plugin: " + pluginName);
         } catch (Exception e) {
@@ -32,7 +25,6 @@ public class PluginManager {
         }
     }
 
-    // Unloads a plugin by removing it from the map
     public void unloadPlugin(String pluginName) {
         if (plugins.containsKey(pluginName)) {
             plugins.remove(pluginName);
@@ -40,7 +32,6 @@ public class PluginManager {
         }
     }
 
-    // Executes the plugin's code by calling its execute method
     public String executePlugin(String pluginName, String input) {
         Plugin plugin = plugins.get(pluginName);
         if (plugin != null) {
